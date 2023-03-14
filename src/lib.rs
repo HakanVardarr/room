8ccd92ca9af7243ba9fa0368ee3e8448081400ee
Hakan Vardar <hakovardo123@gmail.com>
2023-03-14 11:28:25 +0300

    Windows implementation

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,125 @@ pub fn get_screen(width: &mut u32, height: &mut u32, wx: &mut i32, wy: &mut i32)
     buffer
 }
 
-pub fn run(
+pub fn run_win(
+    mut width: u32,
+    mut height: u32,
+    mut wx: i32,
+    mut wy: i32,
+    mut clicked: bool,
+) -> Result<(), String> {
+    let buffer = get_screen(&mut width, &mut height, &mut wx, &mut wy);
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+    let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG)?;
+    let window = video_subsystem
+        .window("rust-sdl2 demo: Video", width, height)
+        .position_centered()
+        .borderless()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().software().build().unwrap();
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture_bytes(&buffer.as_slice())?;
+
+    canvas.copy(&texture, None, None)?;
+    canvas.present();
+
+    'mainloop: loop {
+        for event in sdl_context.event_pump()?.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Option::Some(Keycode::Escape),
+                    ..
+                } => break 'mainloop,
+                Event::MouseButtonDown {
+                    timestamp: _,
+                    window_id: _,
+                    which: _,
+                    mouse_btn: _,
+                    clicks,
+                    x,
+                    y,
+                } => {
+                    clicked = true;
+                    if clicks == 2 {
+                        canvas.copy(&texture, None, None)?;
+                        canvas.present();
+                        clicked = false;
+                    } else {
+                        canvas.copy(
+                            &texture,
+                            Some(Rect::new(x, y, width - 500, height - 500)),
+                            None,
+                        )?;
+                        canvas.present();
+                        wx = x;
+                        wy = y;
+                    }
+                }
+                Event::KeyDown {
+                    timestamp: _,
+                    window_id: _,
+                    keycode,
+                    scancode: _,
+                    keymod: _,
+                    repeat: _,
+                } => {
+                    if clicked {
+                        if wx > 0 && wx < width as i32 - 500 && wy > 0 && wy < height as i32 - 500 {
+                            if let Some(key) = keycode {
+                                match key {
+                                    Keycode::Left => {
+                                        wx -= 30;
+                                    }
+                                    Keycode::Right => {
+                                        wx += 30;
+                                    }
+                                    Keycode::Up => {
+                                        wy -= 30;
+                                    }
+                                    Keycode::Down => {
+                                        wy += 30;
+                                    }
+                                    _ => (),
+                                }
+                            }
+                            canvas.copy(
+                                &texture,
+                                Some(Rect::new(wx, wy, width - 500, height - 500)),
+                                None,
+                            )?;
+                            canvas.present();
+                        } else {
+                            if wx >= width as i32 - 500 {
+                                wx = width as i32 - 501;
+                            }
+                            if wx <= 0 {
+                                wx = 1;
+                            }
+
+                            if wy >= height as i32 - 500 {
+                                wy = height as i32 - 501;
+                            }
+
+                            if wy <= 0 {
+                                wy = 1;
+                            }
+                        }
+                    }
+                }
+
+                _ => {}
+            }
+        }
+    }
+
+    Ok(())
+}
+
+pub fn run_mac(
     mut width: u32,
     mut height: u32,
     mut wx: i32,
@@ -88,7 +206,7 @@ pub fn run(
                     repeat: _,
                 } => {
                     if clicked {
-                        if wx > 0 && wx < width as i32 && wy > 0 && wy < height as i32 {
+                        if wx > 0 && wx < width as i32 - 300 && wy > 0 && wy < height as i32 - 300 {
                             if let Some(key) = keycode {
                                 match key {
                                     Keycode::Left => {
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate sdl2;
 
-use screen_shot::run;
+use screen_shot::{run_mac, run_win};
 
 fn main() -> Result<(), String> {
     let width = 0;
@@ -9,7 +9,20 @@ fn main() -> Result<(), String> {
     let wy = 0;
     let clicked = false;
 
-    run(width, height, wx, wy, clicked)?;
+    let os_type = std::env::consts::OS;
+    match os_type {
+        "macos" => {
+            run_mac(width, height, wx, wy, clicked)?;
+        }
+        "windows" => {
+            run_win(width, height, wx, wy, clicked)?;
+        }
+        _ => {
+            unimplemented!();
+        }
+    }
+
+    // run(width, height, wx, wy, clicked)?;
 
     Ok(())
 }
